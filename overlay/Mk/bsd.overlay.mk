df32197aa1ead782f297323e6cbf888a2057bc0e
Tobias Kortkamp <tobik@FreeBSD.org>
2021-11-23 11:39:48 +0100

    overlay: add guard and try to use shared version in LOCALBASE

--- overlay/Mk/bsd.overlay.mk
+++ overlay/Mk/bsd.overlay.mk
@@ -1,21 +1,27 @@
-.if !defined(DISABLE_PARFETCH) && !make(fetch-list) && !make(fetch-url-list-int) && \
+.if !defined(_INCLUDE_PARFETCH_OVERLAY) && !defined(DISABLE_PARFETCH) && \
+	!make(fetch-list) && !make(fetch-url-list-int) && \
 	!make(fetch-urlall-list) && !make(fetch-url-list)
+_INCLUDE_PARFETCH_OVERLAY=	yes
 
 .if !defined(PARFETCH)
-# Try to use bin/parfetch-static from the overlay if it exists.
-# This makes it available to Poudriere without any further setup.
+.  if exists(${LOCALBASE}/bin/parfetch)
+PARFETCH?=	${LOCALBASE}/bin/parfetch
+.  endif
+# Try to use bin/parfetch-static from the overlay if parfetch
+# is not available from LOCALBASE. This makes it available to
+# Poudriere without any further setup.
 .  for odir in ${OVERLAYS}
 .    if exists(${odir}/bin/parfetch-static)
-PARFETCH=	${odir}/bin/parfetch-static
+PARFETCH?=	${odir}/bin/parfetch-static
 .    endif
 .  endfor
 PARFETCH?=	parfetch
 .endif
 
-# Global connection limit per CURLMOPT_MAX_TOTAL_CONNECTIONS(3)
+# Global connection limit (see CURLMOPT_MAX_TOTAL_CONNECTIONS(3))
 PARFETCH_MAX_TOTAL_CONNECTIONS?=	4
 
-# Max connections per host per CURLMOPT_MAXCONNECTS(3)
+# Max connections per host (see CURLMOPT_MAXCONNECTS(3))
 PARFETCH_MAX_CONNECTS_PER_HOST?=	1
 
 _PARFETCH_ENV=	dp_PARFETCH_MAX_CONNECTS_PER_HOST=${PARFETCH_MAX_CONNECTS_PER_HOST} \
